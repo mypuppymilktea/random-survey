8ed672259070189626aabfe1d8203547410030c9
mypuppymilktea <chi2889377@icloud.com>
2025-03-21 12:37:20 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.surveycake.com/s/abANK',
+	'https://www.surveycake.com/s/86GAY',
+	'www.surveycake.com/s/66Ggd',
 ];
